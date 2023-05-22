011e9d3f282781a049852dc9f4835e5f5a62a2d9
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-05-22 17:59:21 +0530

    Made the Ejs Template and added the pre-made style sheet

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
+let items = ["Buy Food", "Eat", "Cook Food"];
+let workItems = [];
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
-
-
-
+app.get("/", (req, res) => {
+    res.render("ToDoList", {List_Tittle: "Thusday", newItems: items});
+});
 
 app.listen(port, () => console.log(`ToDo app listening on port ${port}!`));
